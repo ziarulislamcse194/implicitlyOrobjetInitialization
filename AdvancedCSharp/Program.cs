633afb7eead5c1419aa656724092d962297435f8
ziarulislamcse194 <ziarulislamcse194@gmail.com>
2019-07-22 23:54:22 +0600

    ImplicitlyOrOnjectInitialize

--- AdvancedCSharp/Program.cs
+++ AdvancedCSharp/Program.cs
@@ -10,6 +10,34 @@ namespace AdvancedCSharp
     {
         static void Main(string[] args)
         {
+            // object initialization
+
+            Student student = new Student()
+            {
+                Id = 1,
+                Name = "A",
+                RegNo = "001"
+
+            };
+
+            // array initialization
+            int[] numbers = {1, 3, 4, 56, 7};
+
+
+            // list initialization
+            List<Student> studentList = new List<Student>()
+            {
+                student,
+                new Student(){Id = 2, Name = "B", RegNo = "002"},
+                new Student(){Id = 3, Name = "C", RegNo = "003"},
+            };
+
+            foreach (Student s in studentList)
+            {
+                Console.WriteLine(s.Id +" "+s.Name+" "+s.RegNo);
+            }
+
+            Console.ReadKey();
         }
     }
 }
---
+++ implicitlyTypedLocalvariable/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdvancedCSharp;
+
+namespace implicitlyTypedLocalvariable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 32;
+            string name = "abcd";
+
+            var number2 = 25;
+            var name2 = "efgh";
+
+
+            //number2 = "dsaw";
+            //name2 = 353;
+
+            var student = new Student();
+            var students = new LinkedList<Student>();
+
+        }
+    }
+}
